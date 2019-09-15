cbdf810efb2fe10fe08b28420dd399e02a6e9d2b
SoBusted <fireintheholl@naver.com>
2019-09-15 16:46:50 +0900

    Add new command "select"

---
+++ commands/select.js
@@ -0,0 +1,19 @@
+const lang = require('../data/lang.json');
+
+module.exports.run = async (bot, message, args) => {
+    if (args.length < 1) {
+        message.reply(lang.lackOfArguments);
+
+        return;
+    }
+
+    message.reply(lang.chooseEnd.random().format(args.random()));
+};
+
+module.exports.config = {
+    name: 'select',
+    description: "Select one between multiple items",
+    alias: ['choice', 'choose', '골라', '뽑아', '선택'],
+    cooltime: 500,
+    isOwnerOnly: false
+};
