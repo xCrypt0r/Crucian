44375756090275d51e82e55a0ff480177874ab08
SoBusted <fireintheholl@naver.com>
2019-09-22 17:28:53 +0900

    Add new command "shuffle"

---
+++ commands/shuffle.js
@@ -0,0 +1,26 @@
+const lang = require('../data/lang.json');
+
+module.exports.run = async (bot, message, args, tools, options) => {
+    let fetched = options.active.get(message.guild.id);
+    
+    if (!fetched) {
+        message.reply(lang.noMusicPlaying);
+
+        return;
+    }
+
+    let nowPlaying = fetched.queue.shift();
+
+    fetched.queue = fetched.queue.shuffle();
+
+    fetched.queue.unshift(nowPlaying);
+    message.reply(lang.shuffleCompleted.random());
+};
+
+module.exports.config = {
+    name: 'shuffle',
+    description: 'Shuffle songs in queue',
+    alias: ['random', 'randomize', 'sh', '랜덤', '셔플'],
+    cooltime: 2000,
+    isOwnerOnly: false
+};
