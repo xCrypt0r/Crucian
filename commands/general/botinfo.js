2484f259caa95e767bf07cf26adb689e4b83757e
SoBusted <fireintheholl@naver.com>
2019-09-28 22:56:36 +0900

    Update command "botinfo"

--- commands/general/botinfo.js
+++ commands/general/botinfo.js
@@ -1,5 +1,7 @@
 const Command = require('../../interfaces/Command.js');
 const discord = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
 
 class Botinfo extends Command {
     constructor(file) {
@@ -15,10 +17,15 @@ class Botinfo extends Command {
     async run(bot, message) {
         let embed = new discord.RichEmbed()
             .setDescription('Bot Information')
-            .setColor('#15f153')
+            .setColor(0x15f153)
             .setThumbnail(bot.user.displayAvatarURL)
-            .addField('Name', `${bot.user.username}#${bot.user.discriminator}`)
-            .addField('Created at', bot.user.createdAt);
+            .addField('Name', `${bot.user.tag}`, true)
+            .addField('Uptime', moment.duration(bot.uptime).format('D [d], H [h], m [m], s [s]'), true)
+            .addField('Users', bot.users.size.toLocaleString(), true)
+            .addField('Servers', bot.guilds.size.toLocaleString(), true)
+            .addField('Channels', bot.channels.size.toLocaleString(), true)
+            .addField('Discord.js', `v${discord.version}`, true)
+            .addField('Node', process.version, true);
 
         message.channel.send(embed);
     }
