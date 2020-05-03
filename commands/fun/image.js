04ad4d07eaf9fce0e0012e5c9e5561290d43ae26
SoBusted <fireinthehall@naver.com>
2020-05-03 12:44:32 +0900

    Add new module "g-i-s" to "image" command

--- commands/fun/image.js
+++ commands/fun/image.js
@@ -1,6 +1,6 @@
 const Command = require('../../interfaces/Command.js');
 const discord = require('discord.js');
-const request = require('request');
+const gis = require('g-i-s');
 
 class Image extends Command {
     constructor(file) {
@@ -22,26 +22,19 @@ class Image extends Command {
         }
     
         let q = args.join(' ');
-        let url = `https://www.google.me/search?q=${encodeURI(q)}&hl=ko&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiEi8OL_eXgAhWUOnAKHbbZDWoQ_AUIDigB&biw=1313&bih=637`;
-    
-        request({
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36'
-            },
-            uri: url
-        }, (err, res, body) => {
-            let images = body.match(/(?<="ou":").+?(?=")/g) || [];
-    
+
+        gis(q, (err, images) => {   
             if (images.length < 1) {
                 message.reply(bot.lang.imageNotFound.random());
     
                 return;
             }
-    
+	
+	    let image = images.random();
             let embed = new discord.RichEmbed()
                 .setColor('#ff9900')
                 .setTitle(`:frame_photo: **${q}**`)
-                .setImage(images.random());
+                .setImage(image.url);
     
             message.channel.send(embed);
         });
