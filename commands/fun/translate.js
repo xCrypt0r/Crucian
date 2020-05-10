b6630e001ab81a3fcb543284d1714d1ac697ad6d
Ubuntu <ubuntu@ip-172-31-40-7.us-east-2.compute.internal>
2020-05-10 07:26:53 +0000

    Add new command "translate"

---
+++ commands/fun/translate.js
@@ -0,0 +1,44 @@
+const Command = require('../../interfaces/Command.js');
+const discord = require('discord.js');
+const translate = require('@vitalets/google-translate-api');
+
+class Translate extends Command {
+    constructor(file) {
+        super(file, {
+            name: 'translate',
+            description: 'Translate message to another language',
+            usage: 'translate #{language code} #{text}',
+            aliases: ['tr', '번역'],
+            isOwnerOnly: false
+        });
+    }
+
+    async run(bot, message, args) {
+        if (args.length < 2) {
+            message.reply(bot.lang.lackOfArguments);
+    
+            return;
+        }
+        
+        let language = args.shift();
+        let text = args.join(' ');
+
+        translate(text, { to: language }).then(res => {
+            let embed = new discord.MessageEmbed()
+                .setColor(0xf4b400)
+                .setTitle('Click here to see supported languages')
+                .setURL('https://cloud.google.com/translate/docs/languages')
+                .setThumbnail(bot.user.avatarURL())
+                .addField(translate.languages[res.from.language.iso], text)
+                .addField(translate.languages[language], res.text);
+
+            message.channel.send(embed);
+        }).catch(err => {
+            if (err.message.includes('not supported')) {
+                message.channel.send(bot.lang.languageNotSupported.format(language));
+            }
+        });
+    }
+}
+
+module.exports = Translate;
