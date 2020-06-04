83ab7fe723ef3f19fc9923b56d60605eba83f02e
SoBusted <fireintheholl@naver.com>
2020-06-04 11:24:29 +0000

    Set korean as default language

--- interfaces/Crucian.js
+++ interfaces/Crucian.js
@@ -9,7 +9,7 @@ class Crucian extends Client {
         this.logger = new Logger(this);
         this.config = require('../assets/json/config.json');
         this.prefix = this.config.PREFIX;
-        this.lang = require('../assets/json/lang.json');
+        this.lang = require('../assets/json/lang_ko.json');
         this.commands = new Collection();
         this.tools = require('../lib/utils.js');
         this.active = new Map();
