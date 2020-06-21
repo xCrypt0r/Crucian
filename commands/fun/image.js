744107d6f0618940c3aff5480b60e0324e334ea2
SoBusted <fireintheholl@naver.com>
2020-06-21 07:25:46 +0000

    Update "image" command

--- commands/fun/image.js
+++ commands/fun/image.js
@@ -1,6 +1,6 @@
 const Command = require('../../structures/Command.js');
-const discord = require('discord.js');
-const gis = require('g-i-s');
+const { promisify } = require('util');
+const gis = promisify(require('g-i-s'));
 
 class Image extends Command {
     constructor(file) {
@@ -13,23 +13,20 @@ class Image extends Command {
     
             return;
         }
-    
-        let q = args.join(' ');
 
-        gis(q, (err, images) => {   
-            if (images.length < 1) {
-                message.reply(bot.lang.imageNotFound.random());
-    
-                return;
-            }
-	
-            let image = images.random();
-            let embed = new discord.MessageEmbed()
-                .setColor('#ff9900')
-                .setTitle(`:frame_photo: **${q}**`)
-                .setImage(image.url);
-    
-            message.channel.send(embed);
+        let images = await gis(args.join(' '));
+
+        if (images.length < 1) {
+            message.reply(bot.lang.imageNotFound.random());
+
+            return;
+        }
+
+        message.channel.send({
+            files:[{
+                attachment: images.random().url,
+                name: 'image.jpg'
+            }]
         });
     }
 }
