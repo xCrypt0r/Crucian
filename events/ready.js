c8975a42bf0d092f75212c58de98cbb997bafd5c
SoBusted <fireintheholl@naver.com>
2020-07-03 10:43:20 +0000

    Replace hardcoded string

--- events/ready.js
+++ events/ready.js
@@ -7,6 +7,6 @@ module.exports = class extends Event {
 
     async run() {
         this.logger.log(`Logged in as ${this.user.tag}`);
-        this.user.setActivity(`${this.prefix}help to see commands`);
+        this.user.setActivity(this.lang.helpManual.activity.format(this.prefix));
     }
 };
