a249a80b74f07faa41a0e6d6e8e936ece19d6d68
SoBusted <fireintheholl@naver.com>
2020-07-11 04:34:11 +0000

    Fix issue that Crucian add emoji to another bot's message

--- extenders/Message.js
+++ extenders/Message.js
@@ -3,8 +3,8 @@ const { Structures } = require('discord.js');
 module.exports = Structures.extend('Message', Message => class extends Message {
     constructor(...args) {
         super(...args);
-        
-        if (this.hasEmbeds && !this.webhookID) {
+
+        if (this.hasEmbeds && this.author.id === bot.user.id) {
             this.addCloseEmoji();
         }
     }
@@ -12,13 +12,13 @@ module.exports = Structures.extend('Message', Message => class extends Message {
     get hasEmbeds() {
         return this.embeds.length > 0;
     }
-    
+
     addCloseEmoji() {
         let closeEmoji = '❌',
             close = this.createReactionCollector(closeEmoji, { time: 100000 });
-            
+
         this.react(closeEmoji);
-        
+
         close.on('collect', (reaction, user) => {
             if (!user.bot) {
                 this.delete();
