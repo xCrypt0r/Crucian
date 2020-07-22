3ba7456b1eee9784854cf8de53451fa7444e6a9b
SoBusted <fireintheholl@naver.com>
2020-07-22 14:58:06 +0000

    Add pagination to command list

---
+++ dashboard/public/js/index.js
@@ -0,0 +1,40 @@
+let $pagination = $('.pagination'),
+    $table = $('#commands'),
+    maxRows = 10,
+    totalRows = $table.find('tbody tr').length;
+
+$table
+    .find('tbody > tr')
+    .hide()
+    .slice(0, maxRows)
+    .show();
+
+if (totalRows > maxRows) {
+    let pageNum = Math.ceil(totalRows / maxRows);
+
+    for (let i = 1; i <= pageNum; i++) {
+        let $pageItem = $('<li>').addClass('page-item').data('page', i).append(
+                $('<a>').addClass('page-link').attr('href', '#').text(i)
+            );
+
+        $pagination.append($pageItem);
+    }
+}
+
+$pagination.find('li:first-child').addClass('active');
+$pagination.find('li').on('click', function(e) {
+    let pageNum = $(this).attr('data-page'),
+        trIndex = 0;
+
+    $pagination.find('li').removeClass('active');
+    $(this).addClass('active');
+    $table.find('tr:gt(0)').each(function() {
+        trIndex++;
+
+        if (trIndex > (maxRows * pageNum) || trIndex <= ((maxRows * pageNum) - maxRows)) {
+            $(this).hide();
+        } else {
+            $(this).show();
+        }
+    });
+});
